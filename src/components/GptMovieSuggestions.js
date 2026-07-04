21a34846d5fd5a364f99bf4bd4b240c13e88c6b7
shivanisharma2022 <sharmashivani0619@gmail.com>
2026-07-04 23:52:12 +0530

    added movies on gpt search page

--- src/components/GptMovieSuggestions.js
+++ src/components/GptMovieSuggestions.js
@@ -1,9 +1,26 @@
-const GptMovieSuggestions = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
 
-export default GptMovieSuggestions
+const GptMovieSuggestions = () => {
+  const gpt = useSelector((store) => store.gpt);
+  const { movieNames, movieResults } = gpt;
+
+  if (!movieNames?.length) return null;
+
+  return (
+    <div className="relative pb-20 mt-4">
+      <div className="absolute inset-0 bg-black/70" />
+      <div className="relative z-10 pl-4 md:pl-12">
+        {movieNames.map((movieName, index) => (
+          <MovieList
+            key={movieName}
+            title={movieName}
+            movies={movieResults[index]}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default GptMovieSuggestions;
--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -1,15 +1,23 @@
+import { useSelector } from "react-redux";
 import GptSearchBar from "./GptSearchBar";
 import GptMovieSuggestions from "./GptMovieSuggestions";
 import { NETFLIX_BACKGROUND } from "../utils/constant";
 
 const GptSearch = () => {
+  const movieNames = useSelector((store) => store.gpt.movieNames);
+  const hasResults = movieNames?.length > 0;
+
   return (
-    <div>
-      <div className="absolute -z-10">
-        <img src={NETFLIX_BACKGROUND} alt="background" />
+    <div className="relative min-h-screen">
+      <div className="fixed inset-0 -z-10">
+        <img
+          src={NETFLIX_BACKGROUND}
+          alt="background"
+          className="w-full h-full object-cover"
+        />
       </div>
       <GptSearchBar />
-      <GptMovieSuggestions />
+      {hasResults && <GptMovieSuggestions />}
     </div>
   );
 };
